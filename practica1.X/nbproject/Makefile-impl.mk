ca3eb50123c8a10c4c305be003c3c0a96930b431
searleser97 <gabrielsanv97@gmail.com>
2020-03-01 11:07:32 -0600

    updated on 03/01/2020 at 11:07:32 AM

--- practica1.X/nbproject/Makefile-impl.mk
+++ practica1.X/nbproject/Makefile-impl.mk
@@ -24,7 +24,7 @@ CLEAN_SUBPROJECTS=${CLEAN_SUBPROJECTS_${SUBPROJECTS}}
 
 
 # Project Name
-PROJECTNAME=proyecto1.X
+PROJECTNAME=practica1.X
 
 # Active Configuration
 DEFAULTCONF=default
--- practica1.X/nbproject/Package-default.bash
+++ practica1.X/nbproject/Package-default.bash
@@ -10,9 +10,9 @@ CND_CONF=default
 CND_DISTDIR=dist
 TMPDIR=build/${CND_CONF}/${IMAGE_TYPE}/tmp-packaging
 TMPDIRNAME=tmp-packaging
-OUTPUT_PATH=dist/${CND_CONF}/${IMAGE_TYPE}/proyecto1.X.${IMAGE_TYPE}.${OUTPUT_SUFFIX}
-OUTPUT_BASENAME=proyecto1.X.${IMAGE_TYPE}.${OUTPUT_SUFFIX}
-PACKAGE_TOP_DIR=proyecto1.x/
+OUTPUT_PATH=dist/${CND_CONF}/${IMAGE_TYPE}/practica1.X.${IMAGE_TYPE}.${OUTPUT_SUFFIX}
+OUTPUT_BASENAME=practica1.X.${IMAGE_TYPE}.${OUTPUT_SUFFIX}
+PACKAGE_TOP_DIR=practica1.x/
 
 # Functions
 function checkReturnCode
@@ -57,15 +57,15 @@ mkdir -p ${TMPDIR}
 
 # Copy files and create directories and links
 cd "${TOP}"
-makeDirectory ${TMPDIR}/proyecto1.x/bin
+makeDirectory ${TMPDIR}/practica1.x/bin
 copyFileToTmpDir "${OUTPUT_PATH}" "${TMPDIR}/${PACKAGE_TOP_DIR}bin/${OUTPUT_BASENAME}" 0755
 
 
 # Generate tar file
 cd "${TOP}"
-rm -f ${CND_DISTDIR}/${CND_CONF}/package/proyecto1.x.tar
+rm -f ${CND_DISTDIR}/${CND_CONF}/package/practica1.x.tar
 cd ${TMPDIR}
-tar -vcf ../../../../${CND_DISTDIR}/${CND_CONF}/package/proyecto1.x.tar *
+tar -vcf ../../../../${CND_DISTDIR}/${CND_CONF}/package/practica1.x.tar *
 checkReturnCode
 
 # Cleanup
